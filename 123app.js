8dc79d01b260b9a6be7dc7da15ef31b4483f2784
purevdorjg <puujeegees@gmail.com>
2020-08-28 12:01:24 -0700

    ehlel 1234-s copy hiiv

---
+++ 123app.js
@@ -0,0 +1,49 @@
+document.querySelector(".add__btn").addEventListener("click", function () {
+  console.log(
+    document.querySelector(".add__description").value +
+      " " +
+      document.querySelector(".add__value").value
+  );
+});
+
+var uiController = (function () {
+  var x = 100;
+
+  function add(y) {
+    return x + y;
+  }
+
+  return {
+    publicAdd: function (a) {
+      a = add(a);
+      console.log("Боловсруулсан утга : " + a);
+    },
+  };
+})();
+
+var financeController = (function () {
+  console.log("sain uu!");
+})();
+
+var appController = (function (uiController, financeController) {
+  uiController.publicAdd(150);
+})(uiController, financeController);
+
+var hunController = (function () {
+  //     // data encapsulation
+
+  //     // private data
+  var bodol = "Javascript толгой эргүүлмээр юм...";
+  function tsusGuih() {}
+  //     // private function
+  function huchilturugchiigAgaaraasSorjTsusruuOruulah() {}
+
+  return {
+    yarih: function () {
+      bodol = "Javascript бол лаг";
+      huchilturugchiigAgaaraasSorjTsusruuOruulah();
+      tsusGuih();
+      console.log("hi");
+    },
+  };
+})();
